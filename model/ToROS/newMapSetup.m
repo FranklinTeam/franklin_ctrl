e6f22ae9fc9836b79b16626eb5fef67605bbb7a7
pierre-mollard <pierre.mollard19@orange.fr>
2020-05-15 14:20:01 +0200

    New map & lidar (not working)

---
+++ model/ToROS/newMapSetup.m
@@ -0,0 +1,21 @@
+%map = load("map.mat");
+imread('map.pgm')
+
+%Cant Read the map ...from ROS msg... sh!t
+imageNorm = double(ans)/255;
+imageOccupancy = 1 - imageNorm;
+mapi = occupancyMap(imageOccupancy,20);
+
+%Ajust the size to the robot diameter
+mapInflated = copy(mapi);
+robotRadius = 0.1;
+inflate(mapInflated,robotRadius);
+
+%Create a probalistic road map
+prm = mobileRobotPRM;
+prm.Map = mapInflated;
+prm.NumNodes = 50;
+prm.ConnectionDistance = 5;
+
+show(mapInflated)
+save("map.mat")
