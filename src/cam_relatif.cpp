e65a6cf8865935662e9ac241c822276211d96e7e
brieuc blanchet <brieuc.blanchet@gmail.com>
2020-06-22 18:09:51 +0200

    parking_cam

---
+++ src/cam_relatif.cpp
@@ -0,0 +1,209 @@
+#include <string>
+#include <std_msgs/String.h>
+#include <ros/ros.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <turtlebot3_msgs/Camera.h>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/aruco.hpp>
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <boost/algorithm/string.hpp>
+
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+cv::Mat rtvecToRtmat(cv::Vec3d rvec, cv::Vec3d tvec){
+	cv::Mat tmat(3,1,CV_64FC1,tvec);
+	cv::Mat rmat, trmat;
+	double fllist[4] = {0.0, 0.0, 0.0, 1.0};
+	cv::Mat flmat(1, 4, CV_64FC1, fllist);
+	tmat.at<double>(0,0) = tvec[0];
+	tmat.at<double>(1,0) = tvec[1];
+	tmat.at<double>(2,0) = tvec[2];
+	cv::Rodrigues(rvec, rmat);
+	cv::hconcat(rmat, tmat, trmat);
+	cv::vconcat(trmat, flmat, trmat);
+	return trmat;
+}
+
+ros::Publisher markers_pub;
+cv::Mat image;
+
+void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+	try{
+		image = cv_bridge::toCvShare(msg, "bgr8")->image;
+		cv::waitKey(30);
+	}
+	catch (cv_bridge::Exception& e){
+		ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+	}
+}
+
+int main(int argc, char **argv){
+
+	ros::init(argc, argv, "frkl_cam");
+	ros::NodeHandle n;
+	markers_pub = n.advertise<turtlebot3_msgs::Camera>("markers_pos", 1);
+
+	image_transport::ImageTransport it(n);
+	image_transport::Subscriber sub = it.subscribe("~/camera/link/camera/image", 1, imageCallback);
+	
+	/*Dictionary of Aruco markers*/
+	int dictionaryId = 11;
+	cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::PREDEFINED_DICTIONARY_NAME(dictionaryId));
+
+	/*Marker length*/
+	float marker_length_m= 0.05;
+	int numberOfPixelInHeight,numberOfPixelInWidth;
+ 	double heightOfSensor, widthOfSensor;
+   	double focalLength=0.5;
+   	double mx, my, U0, V0;
+   	numberOfPixelInHeight=600;
+    	numberOfPixelInWidth=600;
+ 
+    	heightOfSensor=10;
+    	widthOfSensor=10;
+ 
+   	my=(numberOfPixelInHeight)/heightOfSensor ;
+    	U0=(numberOfPixelInHeight)/2 ;
+ 
+    	mx=(numberOfPixelInWidth)/widthOfSensor;
+    	V0=(numberOfPixelInWidth)/2;
+
+	cv::Mat camera_matrix = (cv::Mat_<double>(3,3)<<focalLength*mx, 0, V0,0,focalLength*my,U0,0,0,1);
+	cv::Mat dist_coeffs = (cv::Mat_<double>(5,1) <<0, 0, 0, 0, 0);
+
+	/*Ids of markers of reference*/
+	//std::vector<int> ids_ref{0,1,2,3};
+	std::vector<std::string> ids_ref;
+	ids_ref.push_back("0");
+	ids_ref.push_back("1");
+	ids_ref.push_back("2");
+	ids_ref.push_back("3");
+
+	/*Position of the origin to the different marker of reference*/
+	//std::vector<cv::Vec3d> pos_ref_origin{{0.0,0.0,0.0}};
+	std::vector<cv::Vec3d> pos_ref_origin;
+	pos_ref_origin.push_back(0.0);
+	pos_ref_origin.push_back(0.0);
+	pos_ref_origin.push_back(0.0);
+	pos_ref_origin.push_back(0.0);
+
+
+	std::ostringstream vector_to_marker;
+
+	ros::Rate loop_rate(60); //60FPS
+	cv::Mat image_resized;
+
+    while (1){
+		ros::spinOnce();
+		if(image.empty()==false){
+
+        	std::vector<int> ids;
+        	std::vector<std::vector<cv::Point2f> > corners;
+        	cv::aruco::detectMarkers(image, dictionary, corners, ids);
+
+        	// if at least one marker detected
+        	if (ids.size() > 0){
+           		cv::aruco::drawDetectedMarkers(image, corners, ids);
+				cv::Mat rmat;
+            	std::vector<cv::Vec3d> rvecs, tvecs;
+				std::vector<cv::Mat> rmats, rtmats;
+            	cv::aruco::estimatePoseSingleMarkers(corners, marker_length_m,camera_matrix, dist_coeffs, rvecs, tvecs);
+
+	    		for(int i=0; i < ids.size(); i++){
+					cv::aruco::drawAxis(image, camera_matrix, dist_coeffs, rvecs[i], tvecs[i], 0.1);
+					rtmats.push_back(rtvecToRtmat(rvecs[i],tvecs[i]));
+	    		}
+//            	if(ids_ref_detection.size() > 0){
+					int tab = 10;
+            		for(int i=0; i < ids.size(); i++){
+						//if(std::find(ids_ref.begin(), ids_ref.end(), ids[i]) != ids_ref.end()){
+							//Position of the robot in the coordinate system of the mobile marker
+							cv::Mat pose_mob (4,1,CV_64FC1);
+							std::vector<double> pose_robot;
+							pose_robot.push_back(0.0);
+							pose_robot.push_back(0.0);
+							pose_robot.push_back(0.0);
+							pose_robot.push_back(0.0);
+
+							pose_mob.at<double>(0,0) = pose_robot[0];
+							pose_mob.at<double>(1,0) = pose_robot[1];
+							pose_mob.at<double>(2,0) = pose_robot[2];
+							pose_mob.at<double>(3,0) = 1.0;
+	
+							//Calcul the position of the robot in the coordinate system of the marker of reference
+							double tx = 0.0;
+							double ty = 0.0;
+							double tz = 0.0;
+							cv::Mat pose_ref = rtmats[i] * pose_mob;
+							tx = pose_ref.at<double>(0,0);
+							ty = pose_ref.at<double>(1,0);
+							tz = pose_ref.at<double>(2,0);
+
+							//Conversion double to string
+
+							std::ostringstream osX;
+							std::ostringstream osY;
+							std::ostringstream osZ;
+							osX << tx;
+							osY << ty;
+							osZ << tz;
+							std::string texteX = osX.str();
+							std::string texteY = osY.str();
+							std::string texteZ = osZ.str();
+							texteX = texteX.substr(0, 7);
+							texteY = texteY.substr(0, 7);
+							texteZ = texteZ.substr(0, 7);
+
+							//Display translation and rotation on screen
+
+							//std::string texte = "ID" + ids[i];
+							cv::putText(image, "ID" + ids[i] , cv::Point(tab, 30), cv::FONT_HERSHEY_SIMPLEX, 1.2, cv::Scalar(0, 252, 124), 1, CV_8S);
+							cv::putText(image, "Translation", cv::Point(tab, 60), cv::FONT_HERSHEY_SIMPLEX, 1.2, cv::Scalar(0, 252, 124), 1, CV_8S);
+							cv::putText(image, texteX + " m", cv::Point(tab, 90), cv::FONT_HERSHEY_SIMPLEX, 1.2, cv::Scalar(0, 0, 255), 1, CV_8S);
+                			cv::putText(image, texteY + " m", cv::Point(tab, 120), cv::FONT_HERSHEY_SIMPLEX, 1.2, cv::Scalar(0, 255, 0), 1, CV_8S);
+                			cv::putText(image, texteZ + " m", cv::Point(tab, 150), cv::FONT_HERSHEY_SIMPLEX, 1.2, cv::Scalar(255, 0, 0), 1, CV_8S);
+
+							tab += 300;
+
+							//Sending coordinates on a topic
+							turtlebot3_msgs::Camera marker_pos;
+							marker_pos.y=tx;
+							marker_pos.x=ty;
+							marker_pos.z=tz;
+							markers_pub.publish(marker_pos);
+						//}
+					}
+//            	}
+//				else{
+//					cv::putText(image, "Pas de reference detectee", cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 1.2, cv::Scalar(0, 252, 124), 1, CV_8S);
+//				}
+        	}
+			else{
+				cv::putText(image, "Pas de marqueur detecte", cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 1.2, cv::Scalar(0, 252, 124), 1, CV_8S);
+			}
+			cv::Size size(1920,1080);
+			cv::resize(image,image_resized,size);
+        	imshow("Pose estimation", image_resized);
+			}else{
+				std::cout << "No image" << std::endl;
+			}
+//		}
+		char key = (char)cv::waitKey(10);
+        if (key == 27){
+			break;
+		}
+		loop_rate.sleep();
+    }
+
+	return 0;
+}
---
+++ src/frkl_auto_parking.cpp
@@ -0,0 +1,223 @@
+#define _USE_MATH_DEFINES
+
+#include <string>
+#include <std_msgs/String.h>
+#include <std_msgs/Float32.h>
+#include <ros/ros.h>
+#include <tf/tf.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Pose2D.h>
+#include <nav_msgs/Odometry.h>
+#include <turtlebot3_msgs/Sound.h>
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+#include <algorithm>
+#include <std_msgs/Bool.h>
+#include <turtlebot3_msgs/Camera.h>
+
+ros::Publisher cmd_pub;
+ros::Publisher info_pub;
+ros::Publisher sound_pub;
+
+double targetX = 1;
+double targetY = 1;
+double targetZ = 0;
+
+double realX = 0;
+double realY = 0;
+double realZ = 0;
+
+double initX = 0;
+double initY = 0;
+double initZ = 0;
+
+double angleZ = 0.45;
+
+int ids;
+float MarksX = 0.0;
+float MarksY = 0.0;
+float MarksZ = 0.0;
+
+double BURGER_MAX_LIN_VEL = 0.22; //double BURGER_MAX_LIN_VEL = 0.22;
+double BURGER_MAX_ANG_VEL = 1.5; //double BURGER_MAX_ANG_VEL = 2.84;
+double LIN_VEL_STEP_SIZE = 0.01;
+double ANG_VEL_STEP_SIZE = 0.1;
+
+float angular_speed = 1;
+float linear_speed = 1;
+
+const double PI = 3.14159265358979323846;
+
+bool working = false;
+bool find_marker = false;
+int state = 0;
+double initial_path_distance = 1;
+
+void pubPercentage(float value){
+  std_msgs::Float32 msg;
+  msg.data = value;
+  info_pub.publish(msg);
+}
+
+void stop_Callback(const std_msgs::Bool b){
+  if(b.data){
+    working = false;
+    geometry_msgs::Twist twist;
+    twist.angular.z = 0;
+    twist.linear.x = 0;
+    cmd_pub.publish(twist);
+
+    ROS_INFO("EMERG STOP");
+
+    //turtlebot3_msgs::Sound msg;
+    //msg.value = 3;
+    //sound_pub.publish(msg);
+  }else{
+
+  }
+
+}
+
+void park_Callback(const std_msgs::Bool b){
+    if(b.data){
+	//targetX = MarksX;
+    	//targetY = MarksY;
+	//targetZ = MarksZ;
+	targetX = 1.0;
+    	targetY = 1.0;
+	targetZ = 0.5;
+    //if (Id = 2){
+//	targetZ=0.35;
+    	initX = realX;
+    	initY = realY;
+    	initZ = realZ;
+    	working = true;
+    	state = 0;
+    	ros::Rate loop_rate(10);
+	//path_distance = sqrt((targetX - posX)*(targetX - posX)  + (targetY - posY)*(targetY - posY));
+
+    	while(working){
+           ros::spinOnce();
+           if(working){
+          	//float posT = realT;
+          	//float posX = realX;
+          	//float posY = realY;
+          	geometry_msgs::Twist twist;
+
+          	if(state == 0){ 
+//Détecter s'il y a des obstacles sur la trajectoire du robot
+//Déterminer la position du parking et ce mettre dans l'axe.
+//Se rendre au parking
+			//while(find_marker==false){
+				
+			//}		
+			ROS_INFO("State 0 \n");	
+			twist.linear.x= 0;		 
+			twist.angular.z = angular_speed;
+			ROS_INFO("z = %0.2f \n, realZ");
+		
+            		if(std::abs(targetZ-realZ) <= 0.4){
+              			state = 1;
+	      			twist.angular.z = 0;
+            		}
+
+          	}else if(state == 1){  //Avancer de x-x' (on avance jusqu'à avoir la longueur)
+			ROS_INFO("State 1 \n");	    		
+			twist.angular.z = 0;
+            		twist.linear.x = linear_speed * std::abs(targetX-realX);
+            		twist.linear.x = std::min(twist.linear.x, BURGER_MAX_LIN_VEL);
+
+            		if(std::abs(targetX-realX) <= 0.01){
+              			state = 2;
+	      			twist.linear.x = 0;
+            		}
+          	}else if(state == 2){ //Tourner de 45° en fonction de y positif ou négatif
+			ROS_INFO("State 2 \n");	    		
+			twist.linear.x = 0;
+	    		twist.angular.z = angular_speed * ((targetY-realY)/std::abs(targetY-realY));
+
+            		if(std::abs(angleZ+targetZ-realZ) <= 0.01){
+              			state = 3;
+	      			twist.angular.z = 0;
+            		}
+	  	}else if(state == 3){ //Avancer de y-y'
+			ROS_INFO("State 3 \n");            		
+			twist.angular.z = 0;
+            		twist.linear.x = linear_speed * std::abs(targetY-realY);
+            		twist.linear.x = std::min(twist.linear.x, BURGER_MAX_LIN_VEL);
+
+            		if(std::abs(targetY-realY) <= 0.01){
+             			state = 4;
+	      			twist.linear.x = 0;
+              			working = false;
+				find_marker = false;
+				ROS_INFO("Fin \n");
+            		}
+          	}
+          	cmd_pub.publish(twist);
+           }
+           loop_rate.sleep();
+  	}
+   }else{
+
+   }
+}
+
+void odom_Callback(const nav_msgs::Odometry odom){
+  /** TODO : CAMERA : get data from frkl_poscalc node**/
+
+    // linear position
+    realX = odom.pose.pose.position.x;
+    realY = odom.pose.pose.position.y;
+
+    // quaternion to RPY conversion
+    tf::Quaternion q(
+        odom.pose.pose.orientation.x,
+        odom.pose.pose.orientation.y,
+        odom.pose.pose.orientation.z,
+        odom.pose.pose.orientation.w);
+    tf::Matrix3x3 m(q);
+    double roll, pitch, yaw;
+    m.getRPY(roll, pitch, yaw);
+
+    // angular position
+    realZ = yaw;
+
+    //ROS_INFO("LECTURE ODOM \n- %0.2f\n- %0.2f\n- %0.2f", realX, realY, realT);
+}
+
+void markers_Callback(const turtlebot3_msgs::Camera markers_pos){
+
+    // position markers
+    MarksX = markers_pos.x;
+    MarksY = markers_pos.y;
+    MarksZ = markers_pos.z;
+
+    // markers identity
+    ids = markers_pos.identity;
+
+    find_marker = true;
+
+}
+
+int main(int argc, char** argv) {
+
+    ros::init(argc, argv, "frkl_auto_parking");
+    ros::NodeHandle n;
+
+    //pubs
+    cmd_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+    info_pub = n.advertise<std_msgs::Float32>("f_info_dest", 1);
+    sound_pub = n.advertise<turtlebot3_msgs::Sound>("sound", 1);
+
+    //subs
+    ros::Subscriber mark_sub = n.subscribe("markers_pos",1, markers_Callback);
+    ros::Subscriber park_sub = n.subscribe("parking", 1, park_Callback);
+    ros::Subscriber odom_sub = n.subscribe("odom", 1, odom_Callback);
+    ros::Subscriber stop_sub = n.subscribe("destination/stop", 1, stop_Callback);
+
+    ros::spin();
+
+    return 0;
+}
